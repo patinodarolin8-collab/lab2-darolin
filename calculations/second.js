8fbc47279371459a7ad3a15254a4b4715334c501
patinodarolin8-collab <patinodarolin8@gmail.com>
2025-11-24 22:35:30 +0000

    lab2

---
+++ calculations/second.js
@@ -0,0 +1,11 @@
+// lab Assignment 2.5 second javaScript file 
+// calculate body Mass Idex (BMI)
+
+const weightkg = 70;              //exaple weight in kilograms
+const heightM = 1.75;          //exaple height in meters 
+
+const bmi = weightkg / (heightM* heightM);
+
+console.log("weight (kg): " , weightkg);
+console.log("Height (m):", heightM);
+console.log ("BMI =", bmi);
---
+++ index.js
@@ -0,0 +1,20 @@
+//Lab Assignment 2.4- Main javascript file 
+
+
+// 1. simple addition
+const a = 10;
+const b=5;
+const sum =a+b 
+console. log("1)sun of a+b=",sum);
+
+
+//2. Area of a rectangle  
+const width = 4;
+const height = 3;
+const area = width* height;
+console.log("2) area  of  rectangle  =", area);
+
+//3. Temperture conversion celsius to fahrenheit
+ const celsius = 25;
+ const fahrenheit = celsius * 9 /  5 +  32;
+ console. log ("3) Temperature in fahrenheit = fahrenheit =" , fahrenheit);
